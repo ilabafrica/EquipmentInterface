141f824225b1920ad84085abe762a29f18730fc2
13enjamin <benjaminodonya@gmail.com>
2018-12-04 13:31:38 +0300

    support for humastar 100 - lfts

--- src/EquipmentInterface.php
+++ src/EquipmentInterface.php
@@ -14,7 +14,7 @@ use App\Models\Instrument;
 use App\Http\Controllers\Controller;
 
 class EquipmentInterface extends Controller
-{
+{   
     public function store(Request $request){
     	
     	/*$rules = [
@@ -186,6 +186,72 @@ class EquipmentInterface extends Controller
                             }
                         }
                 $test->save();
+            } else if ($request->instrument === 'humastar_100'){
+                function comma_separated_to_array($string, $separator = ','){
+                    //Explode on comma
+                    $vals = explode($separator, $string);
+                 
+                    //Trim whitespace
+                    foreach($vals as $key => $val) {
+                        $vals[$key] = trim($val);
+                    }
+                    
+                    //Return empty array if no items found
+                    return array_diff($vals, array(""));
+                }
+
+                function get_string_between($string, $start, $end){
+                    $string = ' ' . $string;
+                    $ini = strpos($string, $start);
+                    if ($ini == 0) return '';
+                    $ini += strlen($start);
+                    $len = strpos($string, $end, $ini) - $ini;
+                    return substr($string, $ini, $len);
+                }
+
+                $results = str_replace('{', "", $request['results']);
+                $results = str_replace('}', "", $results);
+                $results = comma_separated_to_array($results);
+                
+                $instrument = Instrument::where('name',$request->instrument)->first();
+
+                $instrumentmappings = InstrumentMapping::where('instrument_id',$instrument->id)->get();
+                
+                foreach ($results as $result) {
+                    $identifier = substr($result, 0, strpos($result, ":"));
+                    $sub_test = get_string_between($result, ':', '=');
+                    $value = str_replace('=', "", strstr($result, '='));              
+                    foreach ($instrumentmappings as $instrumentmapping) {
+                        $test_type_id = $instrumentmapping->test_type_id;
+                        $test = Test::where('identifier', $identifier)->where('test_type_id', $test_type_id)->first();
+
+                        if($test != NULL){
+                            $test->test_status_id = TestStatus::completed;
+                            
+                            foreach ($test->testType->measures as $measure) {
+                                if($measure->measureType->isFreeText()||$measure->measureType->isNumeric()){
+                                    $instrument_parameters = InstrumentParameters::where('instrument_mapping_id', $instrumentmapping->id)->where('sub_test_id',$sub_test)->first();
+                                    $result = Result::updateOrCreate([
+                                        'measure_id'=> $instrument_parameters->measure_id,
+                                        'test_id'   => $test->id,
+                                        'result'    => $value,
+                                    ]);
+                                    $result->time_entered = date('Y-m-d H:i:s');
+                                    $result->save();
+                                    
+                                }
+                            }
+                            $test->save();
+                        }
+                    }
+                }
             }
+    }
+
+    public function humastar (Request $request){
+        $test = Test::with(['testType' => function($query){
+            $query->where('testType.name',$request['testtype']);
+        }])->with('encounter', 'encounter.patient', 'testType.measures', 'encounter.location', 'encounter.patient.name', 'specimen')->whereTest_status_id(TestStatus::pending)->where("specimen_id", "!=", NULL)->get();
+        return response()->json($test);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::post('receiveTestResults', 'ILabAfrica\EquipmentInterface\EquipmentInterface@store');
+Route::post('receiveTestResults/humastar', 'ILabAfrica\EquipmentInterface\EquipmentInterface@humastar');
